e1fe10aa790635f2bd1381c92d542d9624ef5125
Andres Gonzalez <andresg9108@gmail.com>
2018-10-08 10:04:53 -0500

    Implementando librerias.

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,6 @@
 var g_sRouteTemplate = 'src/template/';
 
 $(function(){
-	let oData = {};
-	loadTemplate('header', 'header', oData);
-	loadTemplate('footer', 'footer', oData);
 });
 
 /*
@@ -18,3 +15,69 @@ function loadTemplate(sRouteTemplate, sTag, oData){
 		$(sTag).html(sTemplate);
 	}
 }
+
+// Start MyDesign
+
+/*
+*/
+function myd_openFloatHeaderSearch(){
+    $('.my-floatheader-search').addClass('is-active');
+    $('.my-floatheader-search #search input').focus();
+}
+
+/*
+*/
+function myd_closeFloatHeaderSearch(){
+    $('.my-floatheader-search').removeClass('is-active');
+}
+
+/*
+*/
+function myd_floatHeaderMenu1(){
+    let oMenu = $(".my-floatheader-menu-1");
+    let iTime = 400;
+
+    if(oMenu.css('display') != 'none'){
+        oMenu.removeClass('is-active');
+    }else{
+        oMenu.addClass('is-active');
+    }
+}
+
+
+/*
+*/
+var autocompletedData = [
+"ActionScript",
+"AppleScript",
+"Asp",
+"BASIC",
+"C",
+"C++",
+"Clojure",
+"COBOL",
+"ColdFusion",
+"Erlang",
+"Fortran",
+"Groovy",
+"Haskell",
+"Java",
+"JavaScript",
+"Lisp",
+"Perl",
+"PHP",
+"Python",
+"Ruby",
+"Scala",
+"Scheme"
+];
+$(function(){
+	$( "#myinput" ).autocomplete({
+	source: autocompletedData
+	});
+	$( "#myinputm" ).autocomplete({
+	source: autocompletedData
+	});
+});
+
+// End MyDesign
