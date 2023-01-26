db7026e800401ccd9a3461ea75a8c0214fa1a72a
Andrés González <andresg9108@outlook.com>
2023-01-26 18:43:49 -0500

    Important correction.

--- src/js/appMain.js
+++ src/js/appMain.js
@@ -6,14 +6,20 @@ var oAppMain = {};
 /*
 */
 oAppMain.loadTemplate = (sRouteTemplate, sTag, oData) => {
-  let sRoute = `${g_sRouteTemplate}${sRouteTemplate}.hbs`
-  let sTemplate = Hbs[sRoute](oData);
-  let isTemplate = (document.querySelector(sTag).getAttribute('data-template') == 'true');
-  let sClassCss = document.querySelector(sTag).getAttribute('data-styles');
-  
-  if(isTemplate){
-    document.querySelector(sTag).setAttribute('class', '');
-    document.querySelector(sTag).setAttribute('class', sClassCss);
-    document.querySelector(sTag).innerHTML = sTemplate;
-  }
+    let oElement = document.querySelector(sTag);
+
+    if(oElement !== null){
+        let sRoute = `${g_sRouteTemplate}${sRouteTemplate}.hbs`
+        let sTemplate = Hbs[sRoute](oData);
+        let isTemplate = (oElement.getAttribute('data-template') === 'true');
+        let sClassCss = oElement.getAttribute('data-styles');
+
+        sClassCss = (sClassCss !== null) ? sClassCss : '';
+
+        if(isTemplate){
+            oElement.setAttribute('class', '');
+            oElement.setAttribute('class', sClassCss);
+            oElement.innerHTML = sTemplate;
+        }
+    }
 }
